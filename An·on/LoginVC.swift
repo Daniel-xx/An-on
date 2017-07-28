5c83f5fdc2606507959076e3cf5b0f8ddd65839d
Daniel-xx <danieltravers78@gmail.com>
2017-07-28 22:08:22 +0100

    Bookmark 1 - No Change Doctor
    NO CHANGE DOCTOR!
    Nothing has been changed yet - commit before bedtime

--- An·on/LoginVC.swift
+++ An·on/LoginVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginVC.swift
 //  An•on
 //
 //  Created by Daniel Travers on 28/07/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
